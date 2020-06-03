9a8bf8a27f585c98150b9600e56a3cd307dba445
HamletShu <ahngo13@naver.com>
2020-06-03 20:45:06 +0900

    add app

---
+++ MyDID_app/app/src/main/java/com/hamletshu/mydid/IntroduceActivity.java
@@ -0,0 +1,14 @@
+package com.hamletshu.mydid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroduceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduce);
+    }
+}
